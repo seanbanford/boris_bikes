8f34f43b694f879c2cc98caf749371e76dad8c08
seanbanford <seanbanford@gmail.com>
2019-03-20 15:36:48 +0000

    New Start

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,20 @@
+require_relative 'bike'
+
 class DockingStation
-  def release_bike
-    puts "bike"
+
+  def initialize
+    @bikes = []
   end
+
+  def release_bike
+    fail 'No bike available' if @bikes.empty?
+    @bikes.pop
+  end
+
+  def dock(bike)
+    fail 'No capacity for bike' if @bikes.count >= 20
+    @bikes << bike
+
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,4 +3,38 @@ require 'docking_station.rb'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
 
+  # describe '#dock' do
+  #   it 'Maximum stock of 20 bikes'
+  # end
+
+
+  describe '#dock' do
+    it 'No capacity for bike' do
+      20.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'No capacity for bike'
+    end
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  describe '#release_bike' do
+    it 'releases working bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'checks for an empty docking station' do
+      expect { subject.release_bike }.to raise_error 'No bike available'
+    end
+  end
 end
