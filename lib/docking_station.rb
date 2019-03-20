63384106632131e8e6c85372d7a55d3477d7de1b
seanbanford <seanbanford@gmail.com>
2019-03-20 15:51:28 +0000

    Private and methods added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,14 +9,24 @@ attr_reader :bikes
   end
 
   def release_bike
-    fail 'No bike available' if @bikes.empty?
+    fail 'No bike available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'No capacity for bike' if @bikes.count >= 20
+    fail 'No capacity for bike' if full?
     @bikes << bike
 
+  end
+
+  private
+
+  def full?
+    bikes.count >= 20
+  end
+
+  def empty?
+    bikes.empty?
   end
 
 end
